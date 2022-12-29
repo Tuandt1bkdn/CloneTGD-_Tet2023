cc06353a145af72643fbc37684064bffa4fc4a9a
Tuan <tuanqxktk5@outlook.com>
2022-12-29 20:14:40 +0700

    first commit - start project

---
+++ tgdd2023/src/components/DefaultLayout/index.js
@@ -0,0 +1,5 @@
+function DefaultLayout() {
+  return <div className=""></div>;
+}
+
+export default DefaultLayout;
---
+++ tgdd2023/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div class="text-3xl font-bold text-red-600 bg-blue-500 h-screen">
+      Home Pages
+    </div>
+  );
+}
+
+export default Home;
