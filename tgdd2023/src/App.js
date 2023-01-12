11ce0cbfa6ed52fb500c235b2e7352b0ad00c3d4
Tuan <tuanqxktk5@outlook.com>
2023-01-12 11:04:34 +0700

    first commit

--- tgdd2023/src/App.js
+++ tgdd2023/src/App.js
@@ -2,13 +2,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import DefaultLayout from "./components/DefaultLayout";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <DefaultLayout>
+                <Home />
+              </DefaultLayout>
+            }
+          />
         </Routes>
       </div>
     </Router>
